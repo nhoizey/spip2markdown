e8a7e15fb915152a4bbb579236f1a32b1bc1f8de
Nicolas Hoizey <nicolas@hoizey.com>
2015-04-12 00:59:01 +0200

    Début de formulaire de configuration

---
+++ lang/spip2markdown_fr.php
@@ -0,0 +1,11 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/depublie/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+  'titre_page_configurer_spip2markdown' => 'Paramétrages de SPIP 2 Markdown',
+  'cfg_titre_parametrages' => 'Paramétrages de SPIP 2 Markdown',
+  'telechargement' => 'Forcer le téléchargement, avec un nom de fichier prédéfini ?',
+  'youtube' => 'Convertir les iframe Youtube ?'
+);
