3b4b3bf2cbf01c2b39c6cbe28eb0ddfbfd81e9cd
Nicolas Hoizey <nicolas@hoizey.com>
2015-05-01 22:15:21 +0200

    OMG! Désolé @maieulr & @ndorigny ! 😕

--- spip2markdown_options.php
+++ spip2markdown_options.php
@@ -14,11 +14,11 @@ function spip2markdown($text) {
   $text = spip2markdown_documents($text);
   $text = spip2markdown_reinserer_code($text, $code);
 
-  if(lire_config("spip2mardown/youtube") == "on") {
+  if(lire_config("spip2markdown/youtube") == "on") {
     $text = spip2markdown_youtube($text);
   }
 
-  if(lire_config("spip2mardown/twitter") == "on") {
+  if(lire_config("spip2markdown/twitter") == "on") {
     $text = spip2markdown_twitter($text);
   }
 
